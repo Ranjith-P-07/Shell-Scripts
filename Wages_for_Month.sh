1393087bd2e0ddcf73cc65cc2a80b25a7ff21f6e
Ranjith-P-07 <Ranjithroy704@gmail.com>
2020-10-23 17:18:08 +0530

    Modified Wages for month

--- Wages_for_Month.sh
+++ Wages_for_Month.sh
@@ -6,17 +6,17 @@ totalSalary=0
 empratePerHr=20
 numWorkingDays=20
 
-for (( day=1; day<=$numWorkingDays; days++ ))
+for (( day=1;day<=$numWorkingDays;day++ ))
 do
 	empcheck=$((RANDOM%3));
-		case $empcheck in
-	        	$isFullTime)
-                		empHrs=8;;
-        		$isPartTime)
-                		empHrs=4;;
-       			 *)
-                		empHrs=0;;
-		esac
+	case $empcheck in
+	        $isFullTime)
+       			empHrs=8;;
+        	$isPartTime)
+               		empHrs=4;;
+       		 *)
+               		empHrs=0;;
+	esac
 
 	salary=$(($empHrs*$empratePerHr))
 	totalSalary=$(($totalSalary+$salary))
