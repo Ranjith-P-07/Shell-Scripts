29bc29140a9af5ffc16b0496681cb12ab8ae21a3
Ranjith-P-07 <Ranjithroy704@gmail.com>
2020-10-24 11:14:11 +0530

    PrimeFactorization

--- Primefactorization.sh
+++ Primefactorization.sh
@@ -5,23 +5,25 @@ index=0
 read -p "Enter the number: " num
 if [[ ($num -ge 100) && ($num -le 999) ]];
 then
-	for((i=2;i<=$num;i++));
+	for((i=2;i<=$num*$num;i++));
 	do
 		while [ $(($num%$i)) == 0 ]
 		do
 		echo $i
+			if [ $((i%2)) -eq 0 ]
+			then
+				even_array[index]=$i
+				index=$((index+1))
+			else
+				odd_array[ index ]=$i
+				index=$((index+1))
+			fi
 		num=$((num/$i))
 		done
 	done
 fi
-rem=$(($num % 2))
-if [ $rem -eq 0 ]
-then
-	even_array[$index]=`printf $num`
-else
-	odd_array[$index]=`printf $num`
-fi
-
+echo "${even_array[@]}"
+echo "${odd_array[@]}"
 reverse=0
 a=$(($num % 10))
 n=$(($num/10))
